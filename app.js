5a61e433672657f3e56ae8712caeb3e28e30acb8
shichao.cao <shichao.cao@freemud.cn>
2018-08-07 15:18:28 +0800

    README

--- app.js
+++ app.js
@@ -9,9 +9,45 @@ wx.config = config
 
 App({
   onLaunch: async function () {
-    // example
+    // example 
+    // wx.pro
+    const getSetting = await wx.pro.getSetting()
+    const getUserInfo = await wx.pro.getUserInfo()
     const location = await wx.pro.getLocation()
+    console.log(getSetting)
+    console.log(getUserInfo)
     console.log(location)
+    
+    // 全部封装
+    const all = await Promise.all([
+      wx.pro.getSetting(),
+      wx.pro.getUserInfo(),
+      wx.pro.getLocation()
+    ])
+    console.log(all)
+
+    // 没分装之前写法
+    wx.getSetting({
+      success(res) {
+        wx.getUserInfo({
+          success: function (res) {
+            wx.getLocation({
+              type: 'wgs84',
+              success: function (res) {
+                var latitude = res.latitude
+                var longitude = res.longitude
+                var speed = res.speed
+                var accuracy = res.accuracy
+              }
+            })
+          }
+        })
+      }
+    })
+
+    
+
+
     // 登录
     // let code = await wx.config.async.login()
     // let login_res = await wx.config.api.login(code)
